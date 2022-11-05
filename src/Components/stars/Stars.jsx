218cd0167180d2926d2a6d831a73c31151aad095
axel david <gtacuenta47@gmail.com>
2022-11-05 11:17:38 -0600

    Parte de las estrellas

--- src/Components/stars/Stars.jsx
+++ src/Components/stars/Stars.jsx
@@ -3,18 +3,21 @@ import styles from "./Stars.module.css";
 import stars from "./StarsArreglo";
 
 function Stars() {
-
   return (
     <div>
       <div className={styles.container}>
         <div className={styles.estrellas}>
           {stars.map((star) => (
-            <div key={star.id}>
-              <img src={star.imagen} alt="star" />
+            <div key={star.id} className={styles.imageEstrellas}>
+              <div className={styles.flexStars}>
+                <img src={star.imagen} />
+              </div>
               <img src={star.rectangulo} alt="rectangulo" />
             </div>
           ))}
         </div>
+        <p className={styles.parrafo}>Tu palabra, no nuestra</p>
+        <p className={styles.parrafos}>After 15 years running The Yacht Week, people have a lot to say about us</p>
       </div>
     </div>
   );
--- src/Components/stars/StarsArreglo.jsx
+++ src/Components/stars/StarsArreglo.jsx
@@ -3,30 +3,31 @@ import rectangulo from "../../assets/rectangle.png";
 
 const stars = [
   {
+    
     id: 1,
-    imagen: {star},
-    rectangulo: {rectangulo},
+    imagen: star,
+    rectangulo: rectangulo,
   },
   {
     id: 2,
-    imagen: {star},
-    rectangulo: {rectangulo},
+    imagen: star,
+    rectangulo: rectangulo,
   },
   {
     id: 3,
-    imagen: {star},
-    rectangulo: {rectangulo},
+    imagen: star,
+    rectangulo: rectangulo,
   },
   {
     id: 4,
-    imagen: {star},
-    rectangulo: {rectangulo},
+    imagen: star,
+    rectangulo: rectangulo,
   },
   {
     id: 5,
-    imagen: {star},
-    rectangulo: {rectangulo},
+    imagen: star,
+    rectangulo: rectangulo,
   },
-]
+];
 
-export default stars
+export default stars;
